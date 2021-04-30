973d27c1ed4be54169d40eb12e00faedf9ef3dc0
mathewseto1 <mathew231@hotmail.com>
2021-04-30 19:56:32 +1200

    resolving issues

--- my-app/src/Components/Input.js
+++ my-app/src/Components/Input.js
@@ -1,4 +1,4 @@
-function Input(className, Placeholder, value, setValue) {
+function Input({className, Placeholder, value, setValue}) {
     return <input placeholder = {Placeholder} className = {className} type = "text" value = {value} onChange = {(e) => setValue(e.target.value)}/>;
 
 }
--- my-app/src/Pages/Home/Home.js
+++ my-app/src/Pages/Home/Home.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Input from "../../components/Input";
+import Input from "../../Components/Input";
 
 export default function Home() {
     const [options, setOptions] = useState([]);
