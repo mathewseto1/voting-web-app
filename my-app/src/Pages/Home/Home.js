54dc03979fd0c31c7d562ff1e6b660dbec073f03
mathewseto1 <mathew231@hotmail.com>
2021-04-30 19:31:04 +1200

    resolving Home js issue

---
+++ my-app/src/Pages/Home/Home.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Input from "../../components/Input";
+
+export default function Home() {
+    const [options, setOptions] = useState([]);
+    const [question, setQuestion] = useState("");
+    const [option, setOption] = useState("");
+
+    return (
+        <div className="homeContainer">
+            <h1 className="heading"> Swipe Poll </h1>
+            <div>
+                <Input
+                    className="questionInput"
+                    placeholder="Type a question..."
+                    value={question}
+                    setValue={setQuestion}
+                />
+
+                <Input
+                    className="optionInput"
+                    placeholder="Add an option..."
+                    value={option}
+                    setValue={setOption}
+                />
+
+                <button
+                    className="addButton"
+                    onClick={() => {
+                        setOptions([...options, option]);
+                    }}
+                >
+                    Add
+                </button>
+
+                <ul>
+                    {options.map((o) => (
+                        <li className="listItem" key={o}>
+                            {o}
+                        </li>
+                    ))}
+                </ul>
+
+                <button className="createButton">Create Poll</button>
+            </div>
+        </div>
+    );
+}
